5197f9b8ece9d3e0a58b6d828a96f8444b6ce4bd
lightsun <978136772@qq.com>
2019-05-29 11:27:20 +0800

    opt

--- util-v1-app/app/src/main/java/com/heaven7/android/util_v1_app/MainActivity.java
+++ util-v1-app/app/src/main/java/com/heaven7/android/util_v1_app/MainActivity.java
@@ -3,11 +3,15 @@ package com.heaven7.android.util_v1_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.heaven7.core.util.Logger;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Logger.d(TAG, "onCreate", "");
     }
 }
