22730675cd11eed32163050495b716c449671a7e
heaven7 <18111646505@163.com>
2019-12-14 17:02:41 +0800

    add StyledText and DimenUtil

---
+++ util-v1-app/util_v1/src/main/java/com/heaven7/core/util/DimenUtil.java
@@ -0,0 +1,35 @@
+package com.heaven7.core.util;
+
+import android.content.Context;
+
+/**
+ * the dimen utils
+ * @author heaven7
+ * @since 1.1.6
+ */
+public final class DimenUtil {
+
+    public static int sp2px(Context context, float spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int px2sp(Context context, int pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, int pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int getDimensionPixelSize(Context context, int dimenId) {
+        return context.getResources().getDimensionPixelSize(dimenId);
+    }
+}
