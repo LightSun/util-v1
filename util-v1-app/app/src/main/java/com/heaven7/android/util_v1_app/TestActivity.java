640a534dbb733d00253fa3c2855a650a3740a0ba
LightSun <donshine723@gmail.com>
2019-12-15 11:34:12 +0800

    test HighLightTextHelper with regular ok

--- util-v1-app/app/src/main/java/com/heaven7/android/util_v1_app/TestActivity.java
+++ util-v1-app/app/src/main/java/com/heaven7/android/util_v1_app/TestActivity.java
@@ -1,22 +1,41 @@
 package com.heaven7.android.util_v1_app;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.heaven7.core.util.HighLightTextHelper;
 import com.heaven7.core.util.MainWorker;
 
 public class TestActivity extends AppCompatActivity {
 
+    TextView mTv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_test_entry);
+        mTv = findViewById(R.id.tv);
+
         MainWorker.postDelay(1000, new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(TestActivity.this, MainActivity.class));
+                testHighLightText();
+               // startActivity(new Intent(TestActivity.this, MainActivity.class));
             }
         });
+    }
+
+    private void testHighLightText() {
+        CharSequence text = new HighLightTextHelper.Builder()
+                .setDefaultColor(Color.BLACK)
+                .setHighLightColor(Color.RED)
+                .setRawText("ba0aa1aaa2aaa3")
+                .setHighLightText("a*")
+                .setUseRegular(true)
+                .build().getText();
+        mTv.setText(text);
     }
 }
--- util-v1-app/util_v1/src/main/java/com/heaven7/core/util/HighLightTextHelper.java
+++ util-v1-app/util_v1/src/main/java/com/heaven7/core/util/HighLightTextHelper.java
@@ -3,6 +3,9 @@ package com.heaven7.core.util;
 import android.text.TextUtils;
 import android.widget.TextView;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * the high-light text helper. used to high light text as you like
  * @author heaven7
@@ -14,11 +17,51 @@ public class HighLightTextHelper {
     private String highLightText;
     private int defaultColor;
     private int highLightColor;
+    private boolean useRegular;
+
+    protected HighLightTextHelper(HighLightTextHelper.Builder builder) {
+        this.rawText = builder.rawText;
+        this.highLightText = builder.highLightText;
+        this.defaultColor = builder.defaultColor;
+        this.highLightColor = builder.highLightColor;
+        this.useRegular = builder.useRegular;
+    }
 
     public CharSequence getText(){
         if(TextUtils.isEmpty(highLightText)){
             return rawText;
         }
+        if(useRegular){
+            return getTextByRegular();
+        }else {
+            return getSimpleText();
+        }
+    }
+
+    private CharSequence getTextByRegular() {
+        Matcher matcher = Pattern.compile(highLightText).matcher(rawText);
+        if(matcher.find()){
+            StyledText st = new StyledText();
+            int lastEnd = 0;
+            do {
+                String text = matcher.group();
+                int start = matcher.start();
+                int end = matcher.end();
+                if(start > lastEnd){
+                    st.appendForeground(rawText.substring(lastEnd, start), defaultColor);
+                }
+                st.appendForeground(text, highLightColor);
+                lastEnd = end;
+            }while (matcher.find());
+            if(lastEnd < rawText.length() - 1){
+                st.appendForeground(rawText.substring(lastEnd + 1), defaultColor);
+            }
+            return st;
+        }
+        return rawText;
+    }
+
+    private CharSequence getSimpleText(){
         int i = rawText.indexOf(highLightText);
         if(i >= 0){
             StyledText st = new StyledText();
@@ -36,20 +79,12 @@ public class HighLightTextHelper {
             return rawText;
         }
     }
-
     private String setText(StyledText st, String rawText, int index) {
         if(index > 0){
             st.appendForeground(rawText.substring(0, index), defaultColor);
         }
         st.appendForeground(highLightText, highLightColor);
         return rawText.substring(index + highLightText.length());
-    }
-
-    protected HighLightTextHelper(HighLightTextHelper.Builder builder) {
-        this.rawText = builder.rawText;
-        this.highLightText = builder.highLightText;
-        this.defaultColor = builder.defaultColor;
-        this.highLightColor = builder.highLightColor;
     }
 
     public String getRawText() {
@@ -68,11 +103,16 @@ public class HighLightTextHelper {
         return this.highLightColor;
     }
 
+    public boolean isUseRegular() {
+        return this.useRegular;
+    }
+
     public static class Builder {
         private String rawText;
         private String highLightText;
         private int defaultColor;
         private int highLightColor;
+        private boolean useRegular;
 
         public Builder setRawText(String rawText) {
             this.rawText = rawText;
@@ -88,13 +128,14 @@ public class HighLightTextHelper {
             this.defaultColor = defaultColor;
             return this;
         }
-        public Builder setDefaultColorFrom(TextView tv) {
-            this.defaultColor = tv.getCurrentTextColor();
-            return this;
-        }
 
         public Builder setHighLightColor(int highLightColor) {
             this.highLightColor = highLightColor;
+            return this;
+        }
+
+        public Builder setUseRegular(boolean useRegular) {
+            this.useRegular = useRegular;
             return this;
         }
 
