9caa16152e4ac56381c983c921858b04f4926de5
heaven7 <18111646505@163.com>
2020-08-07 15:29:20 +0800

    opt

--- util-v1-app/util_v1/src/main/java/com/heaven7/core/util/LeakyStorage.java
+++ util-v1-app/util_v1/src/main/java/com/heaven7/core/util/LeakyStorage.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * save object to this storage
@@ -25,7 +26,11 @@ public final class LeakyStorage<T> {
     private final ArrayList<T> sStorage = new ArrayList<>();
     @GuardedBy("mLock")
     private final ArrayMap<T, Integer> sTypefaceMap = new ArrayMap<T, Integer>();
+    private final AtomicInteger mAliveCount = new AtomicInteger(0);
 
+    public int getAliveCount(){
+        return mAliveCount.get();
+    }
     /**
      * save the key-info to bundle and save whole object to this.
      * @param obj the object to save.
@@ -42,6 +47,7 @@ public final class LeakyStorage<T> {
                 id = sStorage.size();
                 sStorage.add(obj);
                 sTypefaceMap.put(obj, id);
+                mAliveCount.incrementAndGet();
             }
             parcel.putInt(KEY_ID, id);
         }
@@ -53,13 +59,33 @@ public final class LeakyStorage<T> {
      * @return the object.
      */
     public @Nullable T restore(@NonNull Bundle parcel) {
+        return restore(parcel, true);
+    }
+
+    /**
+     * restore the object from bundle.
+     * @param parcel the bundle which saved key-info
+     * @param removeFromStorage true if remove from storage
+     * @return the object.
+     * @since 1.1.8
+     */
+    public @Nullable T restore(@NonNull Bundle parcel, boolean removeFromStorage) {
         final int pid = parcel.getInt(KEY_PROCESS_ID);
         final int saveId = parcel.getInt(KEY_ID);
         if (pid != android.os.Process.myPid()) {
             return null;  // The object was created and written in another process.
         }
         synchronized (mLock) {
-            return sStorage.get(saveId);
+            T t = sStorage.get(saveId);
+            if(removeFromStorage){
+                sStorage.set(saveId, null);
+                sTypefaceMap.remove(t);
+                if(mAliveCount.decrementAndGet() <= 0){
+                    sStorage.clear();
+                    sTypefaceMap.clear();
+                }
+            }
+            return t;
         }
     }
 }
